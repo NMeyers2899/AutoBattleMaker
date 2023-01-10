0af34ed0d4f8859ad3c710902497c70c7795459c
Nicholas Meyers <nmeyers2899@gmail.com>
2023-01-10 16:22:40 -0600

    Changed how the Act Task Checks for a Target

--- Source/AutoBattleMaker/BTTask_Act.cpp
+++ Source/AutoBattleMaker/BTTask_Act.cpp
@@ -9,10 +9,10 @@ EBTNodeResult::Type UBTTask_Act::ExecuteTask(UBehaviorTreeComponent& OwnerComp, 
 {
 	AUnitCharacter* unit = dynamic_cast<AUnitCharacter*>(OwnerComp.GetAIOwner()->GetPawn());
 
-	if (!unit->GetTarget())
+	if (!unit->GetTarget() || unit->GetTarget() == unit)
 		return EBTNodeResult::Failed;
 
-	if (unit->GetTarget() != unit && FVector::Dist(unit->GetActorLocation(), unit->GetTarget()->GetActorLocation()) < unit->GetUnitStats().ActionRange)
+	if (FVector::Dist(unit->GetActorLocation(), unit->GetTarget()->GetActorLocation()) < unit->GetUnitStats().ActionRange)
 	{
 		unit->Act();
 		return EBTNodeResult::Succeeded;
